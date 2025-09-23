3321c986b6c000209569e831dd02007b46bd805b
Gustavo Barbosa <abguto.06@gmail.com>
2025-09-23 18:04:06 -0300

    erro de usuario

--- app/(screens)/mapa/index.tsx
+++ app/(screens)/mapa/index.tsx
@@ -1,21 +1,30 @@
 import React from 'react';
 import MapView from 'react-native-maps';
 import { StyleSheet, View } from 'react-native';
+import { Button, ButtonText } from '@/components/ui/button';
+import { useRouter } from 'expo-router';
 
 export default function App() {
+    const router = useRouter();
   return (
     <View style={styles.container}>
       <MapView style={styles.map} />
+      <Button
+      onPress={() => router.push('/')}
+      >
+        <ButtonText>Sair</ButtonText>
+      </Button>
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
   },
   map: {
-    width: '100%',
-    height: '100%',
+    width: '90%',
+    height: '90%',
   },
 });
